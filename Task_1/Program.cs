0def619034b6b9ac3001d4a891a1bc9177977928
Xenia <listk98@yandex.ru>
2022-04-10 16:38:22 +0300

    Add Task_1.1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -10,10 +10,10 @@ double[,] RandomArray (int row, int column)
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
-		{
+        {
             array[i,j] = -10 + new Random().NextDouble() * 100;
         }
-	}
+    }
     return array;
 }
 
@@ -22,7 +22,7 @@ void PrintArray(double[,] array)
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
-		{
+        {
             Console.Write($"{array[i,j]: 0.000}\t");
         }
         Console.WriteLine();
