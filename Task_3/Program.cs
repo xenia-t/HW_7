6f693473abc97a161b8aa63808f183023e8f10fc
Xenia <listk98@yandex.ru>
2022-04-10 18:31:20 +0300

    Add Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] array = RandomArray(3,4);
+Console.WriteLine();	
+PrintArray(array);
+
+double[] average = FindAverage(array);
+Console.Write("Среднее арифметическое в каждом столбце:");
+PrintAverage(average);
+
+
+
+int[,] RandomArray (int row, int column)
+{
+    int[,] array = new int[row,column];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(1,10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine("\n");
+    }
+}
+
+double[] FindAverage (int[,] array)
+{
+    double[] average = new double[array.GetLength(1)];
+    double sum = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i,j];
+        }
+        average [j] = sum / array.GetLength(0);
+        sum = 0;        
+    }
+    return average;
+}
+
+void PrintAverage(double[] average)
+{
+    foreach (double digit in average)
+    {
+        Console.Write($"{digit: 0.0} ");
+    }
+}
